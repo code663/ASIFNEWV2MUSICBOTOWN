cc67c80bc375e0684db0ab2b898f3cddc4896fe8
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-02-10 09:53:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ASIFXQUEEN/ASIFNEWV2MUSICBOTOWN",
+    "https://github.com/code663/ASIFNEWV2MUSICBOTOWN.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
